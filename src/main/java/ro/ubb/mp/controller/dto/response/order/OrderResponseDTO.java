364ef8cedd21d56dd3e9269a9e546b81dd13d3dc
Zimbru Florin <florinz@cicadatech.eu>
2024-12-04 23:24:08 +0200

    commit

--- src/main/java/ro/ubb/mp/controller/dto/response/order/OrderResponseDTO.java
+++ src/main/java/ro/ubb/mp/controller/dto/response/order/OrderResponseDTO.java
@@ -2,10 +2,14 @@ package ro.ubb.mp.controller.dto.response.order;
 
 import lombok.Builder;
 import lombok.Data;
+import ro.ubb.mp.dao.model.OrderItem;
+
+import java.util.List;
 
 @Data
 @Builder
 public class OrderResponseDTO {
     private Long id;
     private String name;
+    private List<OrderItem> orderItems;
 }
